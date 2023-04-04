12d58cdde564c24468bbfac5c06985f768b78889
catewairimu <catewairimu173@gmail.com>
2023-04-04 14:22:45 -0700

    reserved

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse linked list
+ * @head:first node of a list.
+ *
+ * Return: pointer to the first node of the reserved list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *p;
+	listint_t *n;
+
+	p = NULL;
+	n = NULL;
+
+	while (*head != NULL)
+	{
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
+	}
+
+	*head = p;
+	return (*head);
+}
