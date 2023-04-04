f8cd90fab208aebf893c4b9d2199769eedf2be43
catewairimu <catewairimu173@gmail.com>
2023-04-04 12:22:09 -0700

    list1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,20 @@
 #include "lists.h"
+
 /**
- *print_listint - prints all elements of a list
- *@h: head of a list.
+ * print_listint - prints elements of a list
+ * @h: head of list
  *
- *Return: numbers of the nodes.
- *
+ * Return: numbers of the nodes
+ */
 size_t print_listint(const listint_t *h)
 {
-size_t nnodes = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
-	printf("%d\n", h->n);
-	h = h->next;
-	nnodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
 	}
 	return (nnodes);
-	}
+}
