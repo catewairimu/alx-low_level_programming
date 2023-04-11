c602ec75c73d23e4ae9a468a6f9278da8dc5b34e
catewairimu <catewairimu173@gmail.com>
2023-04-11 11:55:59 -0700

    textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it 
+ * read_textfile - reads a text file and prints it
  * to the POSIX standard output.
  * @filename: filename.
  * @letters: numbers of letters printed.
